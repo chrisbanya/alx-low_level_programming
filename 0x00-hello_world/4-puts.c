7c4210905f4438821a404f64850495803f478482
chrisbanya <adabanyachristian21@gmail.com>
2023-07-13 15:54:29 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - A code that prints a line with puts function
+ * Return: 0 (pass)
+ */
+int main(void)
+{puts('\"Programming is like building a multilingual puzzle');
+return (0); }
