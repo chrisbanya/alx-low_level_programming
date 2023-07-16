88e3fe92fe41ab5f594079ef6fe9096bd887e99d
chrisbanya <adabanyachristian21@gmail.com>
2023-07-16 07:40:03 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art," follwed new line
+ * main - Prints the size of the various type based on the computer it is compiled and run on..
  * Return: Always 0.
- *
  */
 
 int main(void)
 
 {
-	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	 printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+	 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	 printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	 printf("Size of a float %zu byte(s)\n", sizeof(float));
 	 return (0);
-
 }
 
