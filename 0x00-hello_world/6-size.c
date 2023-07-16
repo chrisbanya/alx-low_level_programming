b095fc37a6e089f9d1901aa8450a131cf88a5ec7
chrisbanya <adabanyachristian21@gmail.com>
2023-07-16 06:56:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints "with proper grammar, but the outcome is a piece of art," follwed new line
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+
+{
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	 return (0);
+
+}
+
